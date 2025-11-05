4ca0d08f0e134302ac92079def68e552fc2873de
Nasim Koohestani <nasimkoohestani82@gamil.com>
2025-11-05 21:21:47 +0330

    feat(sliding-window): implement Best Time to Buy and Sell Stock solution

--- Sliding Window/Besttimetosellandbuy.py
+++ Sliding Window/Besttimetosellandbuy.py
@@ -1,14 +1,10 @@
-from typing import List
-
-
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+class Solution(object):
+    def maxProfit(self, prices):
         min_price=float('inf')
         max_profit=0
         for price in prices:
             if price < min_price:
                 min_price=price
-            profit=price-min_price
-            if profit > max_profit:
-                profit=max_profit
+                if price - min_price > max_profit:
+                    max_profit=price - min_price
         return max_profit
